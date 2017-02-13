7186b193db2c0ba6c7cd98b6b4e852bd138b9209
ivan-krot <i.m.krot@ukr.net>
2017-02-13 23:20:00 +0200

    404

--- wp-content/themes/softgroup/404.php
+++ wp-content/themes/softgroup/404.php
@@ -14,7 +14,7 @@ get_header(); ?>
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'softgroup' ); ?></h1>
+					<h1 class="page-title"><?php esc_html_e( 'Ooooooooooooops! That page can&rsquo;t be found.', 'softgroup' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
